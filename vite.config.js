448bcb32309ce3e74737934beaa8c0f3cd48a3e8
Bohdan <yulia.pakhachuk@gmail.com>
2023-03-29 16:37:00 +0200

    fix routg

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/vue-zip-code/',
   plugins: [vue()],
   resolve: {
     alias: {
